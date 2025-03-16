7258321902d92eb97d12036fe51b12e0fd108e8e
Roman Emiliano Arenas <98709881+RomanArenas32@users.noreply.github.com>
2025-03-15 20:19:23 -0500

    session

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,7 @@
 import AdminHome from "@/components/admin/home";
-import NotFound from "@/components/not-found/notFound";
 import { getSession } from "@/lib/auth";
 import { Role } from "@/models/api";
+import { redirect } from "next/navigation";
 
 // Forzar renderizado dinámico en el servidor
 export const dynamic = "force-dynamic";
@@ -10,8 +10,9 @@ export default async function Page() {
   const session = await getSession();
   
   // Verifica si el usuario tiene rol ADMIN
-  if (session.user?.role !== Role.ADMIN) {
-    <NotFound/> // Redirige a la página 404 si el rol no es ADMIN
+  if (!session.isLoggedIn || session.user?.role !== Role.ADMIN) {
+    // Usar redirect en lugar de renderizar NotFound
+    redirect("/404");
   }
 
   // Si el rol es ADMIN, renderiza la página del admin
